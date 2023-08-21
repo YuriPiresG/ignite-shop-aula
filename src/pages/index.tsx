ea1ac0ba206c370045714abf138ab1f92072af6a
Yuri Pires <yuripiresbr@gmail.com>
2023-08-21 10:40:40 -0300

    integrando stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,76 @@
-export default function Home() {
-  return <h1>Hello World</h1>;
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
+
+import { useKeenSlider } from "keen-slider/react";
+
+import camiseta1 from "../assets/camisetas/1.png";
+import camiseta2 from "../assets/camisetas/1.png";
+import camiseta3 from "../assets/camisetas/1.png";
+
+import "keen-slider/keen-slider.min.css";
+import { useEffect } from "react";
+import { stripe } from "../lib/stripe";
+import { GetServerSideProps } from "next";
+import Stripe from "stripe";
+
+interface HomeProps {
+  products: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: number;
+  }[];
 }
+
+export default function Home({ products }: HomeProps) {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+  return (
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      {products.map((product) => {
+        return (
+          <Product key={product.id} className="keen-slider__slide">
+            <Image
+              src={camiseta1}
+              alt={product.name}
+              width={520}
+              height={480}
+            />
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+        );
+      })}
+    </HomeContainer>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await stripe.products.list({
+    expand: ["data.default_price"],
+  });
+
+  const products = response.data.map((product) => {
+    const price = product.default_price as Stripe.Price;
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: price.unit_amount! / 100,
+    };
+  });
+
+  console.log(response);
+
+  return {
+    props: {
+      list: [1, 2, 3],
+    },
+  };
+};
